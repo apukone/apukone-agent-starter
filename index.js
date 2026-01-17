afeb31f1fb95b40faaeceaa1403b9b9c34245ad0
Miika von Bell <miika.vonbell@outlook.com>
2026-01-17 15:52:46 +0200

    stats

--- index.js
+++ index.js
@@ -3,8 +3,11 @@ import ollama from 'ollama';
 import 'dotenv/config';
 
 const processMessage = async (data) => {
-  const { message } = await ollama.chat({ model: process.env.OLLAMA_MODEL, messages: data });
-  return message;
+  const response = await ollama.chat({ model: process.env.OLLAMA_MODEL, messages: data });
+  return {
+    ...response.message,
+    eval_count: response.eval_count
+  };
 };
 
 ApukoneClient({ host: process.env.HOST, token: process.env.TOKEN, onMessage: processMessage });
